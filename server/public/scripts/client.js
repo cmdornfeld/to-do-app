b31d27c2a895ab1a901adb374d7824c47569c20b
Chad Dornfeld <cmdornfeld@gmail.com>
2019-11-22 17:28:34 -0600

    added onReady and addTask functions; verified they work

---
+++ server/public/scripts/client.js
@@ -0,0 +1,11 @@
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('JQ');
+    $('#submit-btn').on('click', addTask);
+}
+
+function addTask() {
+    console.log('in addTask');
+    
+}
